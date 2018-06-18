6db87b84c7ac5484171e594f23780c6c67a2d75e
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-06-18 14:19:51 +0300

    s

--- Cash/Cash/App.xaml.cs
+++ Cash/Cash/App.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+
 
 namespace Cash
 {
