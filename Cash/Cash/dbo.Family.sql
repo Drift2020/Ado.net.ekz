fa7915d13b05218398f24cdfa4c680f828f3d246
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-06-19 10:18:04 +0300

    db local

---
+++ Cash/Cash/dbo.Family.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Family]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [name] NVARCHAR(50) NOT NULL
+)
---
+++ Cash/Cash/dbo.Link.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Link_People_Family]
+(
+	[PeopleID] INT NULL,
+    [FamilyID] INT NULL, 
+    CONSTRAINT [FK_Link_To_People] FOREIGN KEY ([PeopleID]) REFERENCES [People](id), 
+    CONSTRAINT [FK_Link_To_Family_ToTable] FOREIGN KEY ([FamilyID]) REFERENCES [Family](id) 
+)
