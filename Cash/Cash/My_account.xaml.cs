6219f96fe91dc551746332c6ebb95ff83c8220c3
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-06-25 18:12:07 +0300

    сделал внешний вид опций профиля начал сравнениек месяцев

---
+++ Cash/Cash/My_account.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cash
+{
+    /// <summary>
+    /// Interaction logic for My_account.xaml
+    /// </summary>
+    public partial class My_account : Window
+    {
+        public My_account()
+        {
+            InitializeComponent();
+        }
+    }
+}
