e19bdb9691754bb300c3bb8370479f2b92ab59f9
Mamory <32417036+Drift2020@users.noreply.github.com>
2018-06-27 22:56:26 +0300

    Edit warpanal

--- Cash/Cash/Index.xaml.cs
+++ Cash/Cash/Index.xaml.cs
@@ -69,6 +69,10 @@ namespace Cash
             {
                 e.Cancel = true;
             }
+            if (!messege_view_Model.is_ok)
+            {
+                e.Cancel = true;
+            }
 
         }
         
